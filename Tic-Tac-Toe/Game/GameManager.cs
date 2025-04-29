91e2e32d34a295448211885aa5567f57683d6408
Diego200509 <diegojijon2005@gmail.com>
2025-04-28 21:24:45 -0500

    Se añadio la clase GameManager para ser la clase principal.

---
+++ Tic-Tac-Toe/Game/GameManager.cs
@@ -0,0 +1,28 @@
+﻿namespace Tic_Tac_Toe.Game
+{
+    public class GameManager
+    {
+        private Board _board;
+        public IPlayer CurrentPlayer { get; set; }
+
+        public GameManager()
+        {
+            _board = new Board();
+            // Aquí puedes agregar cualquier tipo de jugador sin modificar la clase GameManager
+            CurrentPlayer = new HumanPlayer("Player 1", "X");
+        }
+
+        public void SwitchPlayer()
+        {
+            if (CurrentPlayer is HumanPlayer)
+                CurrentPlayer = new ComputerPlayer("Computer", "O");  // Cambiar a un jugador de computadora
+            else
+                CurrentPlayer = new HumanPlayer("Player 1", "X");  // Cambiar a un jugador humano
+        }
+
+        public void MakeMove(string position)
+        {
+            _board.UpdateBoard(position, CurrentPlayer.Symbol);
+        }
+    }
+}
