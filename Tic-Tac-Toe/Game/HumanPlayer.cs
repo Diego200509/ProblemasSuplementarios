6f386c4ed71a59321d550a39ac01e247a7edd90e
Diego200509 <diegojijon2005@gmail.com>
2025-04-28 21:17:33 -0500

    se añadio el humanPlayer que extiende de IPlayer.

---
+++ Tic-Tac-Toe/Game/HumanPlayer.cs
@@ -0,0 +1,20 @@
+﻿namespace Tic_Tac_Toe.Game
+{
+    public class HumanPlayer : IPlayer
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+
+        public HumanPlayer(string name, string symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+
+        public void MakeMove(Board board)
+        {
+            // Lógica para hacer un movimiento como jugador humano
+            // Ejemplo: el humano selecciona una casilla en el tablero
+        }
+    }
+}
