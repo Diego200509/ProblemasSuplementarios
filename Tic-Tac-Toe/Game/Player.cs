f9d925ee7ac75299c7b9ac28c727f6ef6cd03f34
Diego200509 <diegojijon2005@gmail.com>
2025-04-28 21:11:40 -0500

    Se añadio la clase Player y ademas la creacion del formulario.

---
+++ Tic-Tac-Toe/Game/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace Tic_Tac_Toe.Game
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+
+        public Player(string name, string symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+    }
+
+}
