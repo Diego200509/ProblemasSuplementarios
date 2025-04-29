3a7e0dbf0e005dacd2a64aef56ae87f18f94ac7f
Leo538 <lozadaleonel15@gmail.com>
2025-04-29 00:28:26 -0500

    Se agrego los 3 ejercicos por consola

---
+++ Check Protection/ICheckFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Protection
+{
+    // POO: Definición de interfaz (abstracción)
+    public interface ICheckFormatter
+    {
+        string FormatAmount(decimal amount);
+    }
+    }
---
+++ Division/IMultiplier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumberMultiplier
+{
+    public interface IMultiplier
+    {
+        int Multiply(params int[] numbers);
+    }
+}
