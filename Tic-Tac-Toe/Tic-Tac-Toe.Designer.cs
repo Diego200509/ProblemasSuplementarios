15a2217e1ffa20a8621cb25836b89c6d881342d8
Diego200509 <diegojijon2005@gmail.com>
2025-04-28 21:36:05 -0500

    se añadio la tabla y las separaciones para la inferfaz.

--- Tic-Tac-Toe/Tic-Tac-Toe.Designer.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe.Designer.cs
@@ -28,21 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gboxInicio = new System.Windows.Forms.GroupBox();
+            this.gboxTicTac = new System.Windows.Forms.GroupBox();
+            this.tblTicTacToe = new System.Windows.Forms.TableLayoutPanel();
+            this.gboxTicTac.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gboxInicio
+            // 
+            this.gboxInicio.Location = new System.Drawing.Point(12, 12);
+            this.gboxInicio.Name = "gboxInicio";
+            this.gboxInicio.Size = new System.Drawing.Size(231, 454);
+            this.gboxInicio.TabIndex = 0;
+            this.gboxInicio.TabStop = false;
+            // 
+            // gboxTicTac
+            // 
+            this.gboxTicTac.Controls.Add(this.tblTicTacToe);
+            this.gboxTicTac.Location = new System.Drawing.Point(261, 12);
+            this.gboxTicTac.Name = "gboxTicTac";
+            this.gboxTicTac.Size = new System.Drawing.Size(674, 454);
+            this.gboxTicTac.TabIndex = 1;
+            this.gboxTicTac.TabStop = false;
+            // 
+            // tblTicTacToe
+            // 
+            this.tblTicTacToe.ColumnCount = 3;
+            this.tblTicTacToe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblTicTacToe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tblTicTacToe.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tblTicTacToe.Location = new System.Drawing.Point(55, 90);
+            this.tblTicTacToe.Name = "tblTicTacToe";
+            this.tblTicTacToe.RowCount = 3;
+            this.tblTicTacToe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblTicTacToe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblTicTacToe.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblTicTacToe.Size = new System.Drawing.Size(570, 358);
+            this.tblTicTacToe.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(839, 450);
+            this.ClientSize = new System.Drawing.Size(974, 503);
+            this.Controls.Add(this.gboxTicTac);
+            this.Controls.Add(this.gboxInicio);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic-Tac-Toe";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.gboxTicTac.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gboxInicio;
+        private System.Windows.Forms.GroupBox gboxTicTac;
+        private System.Windows.Forms.TableLayoutPanel tblTicTacToe;
     }
 }
 
