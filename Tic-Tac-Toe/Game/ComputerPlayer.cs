1905a90bbfe3a8bdb7b08b1a2378f692a249bd60
Diego200509 <diegojijon2005@gmail.com>
2025-04-28 21:20:05 -0500

    se agrego la clase computerPlayer que extiende de player.

---
+++ Tic-Tac-Toe/Game/ComputerPlayer.cs
@@ -0,0 +1,19 @@
+﻿namespace Tic_Tac_Toe.Game
+{
+    public class ComputerPlayer : IPlayer
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+
+        public ComputerPlayer(string name, string symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+
+        public void MakeMove(Board board)
+        {
+            // Lógica para que la computadora haga un movimiento (puede ser aleatorio o basado en reglas)
+        }
+    }
+}
