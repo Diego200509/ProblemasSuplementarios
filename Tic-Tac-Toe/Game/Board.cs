79d59a97cd9900f6b56adeb48a05cc8a3793b0be
Diego200509 <diegojijon2005@gmail.com>
2025-04-28 21:16:04 -0500

    Se agrego la clase Board y ademas la interfaz se agrego la INterfaz IPlayer.

---
+++ Tic-Tac-Toe/Game/Board.cs
@@ -0,0 +1,19 @@
+﻿namespace Tic_Tac_Toe.Game
+{
+    public class Board
+    {
+        private string[] _board = new string[9]; 
+
+        public void UpdateBoard(string buttonName, string symbol)
+        {
+            int index = int.Parse(buttonName.Substring(buttonName.Length - 1)) - 1;
+            _board[index] = symbol;
+        }
+
+        public bool CheckWinner()
+        {
+          
+            return false;
+        }
+    }
+}
---
+++ Tic-Tac-Toe/Game/IPlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace Tic_Tac_Toe.Game
+{
+    public interface IPlayer
+    {
+        string Name { get; set; }
+        string Symbol { get; set; }
+
+        void MakeMove(Board board);
+    }
+}
