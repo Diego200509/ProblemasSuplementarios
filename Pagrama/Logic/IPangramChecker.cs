8aead0bc6294cc95c9f8bf1979ad67faa6112ae8
Leo538 <lozadaleonel15@gmail.com>
2025-05-28 19:34:41 -0500

    Se agrego Pangrama

---
+++ Pagrama/Logic/IPangramChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagrama.Logic
+{
+    public interface IPangramChecker
+    {
+        bool IsPangram(string sentence);
+        int CountValidCharacters(string sentence);
+    }
+}
---
+++ Pagrama/Logic/PangramChecker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Pagrama.Logic
+{
+    public class PangramChecker : IPangramChecker
+    {
+        private static readonly HashSet<char> SaxonAlphabet = new HashSet<char>("abcdefghijklmnopqrstuvwxyz");
+
+        public bool IsPangram(string sentence)
+        {
+            string normalized = RemoveDiacritics(sentence.ToLower());
+            HashSet<char> foundLetters = new HashSet<char>();
+
+            foreach (char c in normalized)
+            {
+                if (SaxonAlphabet.Contains(c))
+                {
+                    foundLetters.Add(c);
+                }
+            }
+
+            return foundLetters.Count == SaxonAlphabet.Count;
+        }
+
+        public int CountValidCharacters(string sentence)
+        {
+            int count = 0;
+            string normalized = RemoveDiacritics(sentence.ToLower());
+
+            foreach (char c in normalized)
+            {
+                if (SaxonAlphabet.Contains(c))
+                    count++;
+            }
+            return count;
+        }
+
+        private string RemoveDiacritics(string text)
+        {
+            var normalized = text.Normalize(NormalizationForm.FormD);
+            var filtered = new StringBuilder();
+
+            foreach (char c in normalized)
+            {
+                var unicode = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicode != UnicodeCategory.NonSpacingMark)
+                {
+                    filtered.Append(c);
+                }
+            }
+
+            return filtered.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
