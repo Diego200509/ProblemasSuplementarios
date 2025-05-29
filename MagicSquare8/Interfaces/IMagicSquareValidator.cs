02a51eed4462d33a2d10025fd0e168d0e024196f
Diego200509 <diegojijon2005@gmail.com>
2025-05-28 19:55:03 -0500

    Se realizo el juego del cuadro magico con uso correcto de principios SOLID y clean code.

---
+++ MagicSquare8/Interfaces/IMagicSquareValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicSquare8.Interfaces
+{
+    public interface IMagicSquareValidator
+    {
+        bool IsMagic(int?[,] square);
+    }
+}
---
+++ MagicSquare_8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicSquare_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
